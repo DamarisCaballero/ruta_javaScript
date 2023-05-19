f4eb6026b8f05b7420d2ad536f0fab6b3c209c60
DamarisCaballero <damariscallero011@gmail.com>
2023-05-18 20:00:35 -0600

    array

--- array.js
+++ array.js
@@ -8,3 +8,18 @@ for(var i = 0; i < arr.length; i++){
 console.log(arr[i]);
 }
 console.log("indexing complete");
+
+
+/******************************** */
+var arr = [
+    [10, 25, 7],
+    [100, 20, 53],
+    [1, 12, 13]
+   ]; // Initialise arr
+// iterate each element
+for(var i = 0; i < arr.length; i++){
+console.log(arr[i][0]);
+console.log(arr[i][1]);
+console.log(arr[i][2]);
+}
+console.log("indexing complete");
