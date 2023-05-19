5dcff7d5ab403631e9a2e80712659a6ae1f05ded
DamarisCaballero <damariscallero011@gmail.com>
2023-05-18 19:59:47 -0600

    array

---
+++ array.js
@@ -0,0 +1,10 @@
+var arr = [
+    [10, 25, 7],
+    [100, 20, 53],
+    [1, 12, 13]
+   ]; // Initialise arr
+// iterate each element
+for(var i = 0; i < arr.length; i++){
+console.log(arr[i]);
+}
+console.log("indexing complete");
