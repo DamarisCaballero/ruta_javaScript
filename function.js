d7362e5f9164b27a15d4a76b2e13eafe85aa6ca5
DamarisCaballero <damariscallero011@gmail.com>
2023-05-22 14:06:53 -0600

    name
    Define function that prints the argument passed

--- function.js
+++ function.js
@@ -48,3 +48,27 @@ function getName(){
 
 var var3 = getName(); // assign sum to var3
 console.log(var3); // print the value of var3
+
+
+//******************************* */
+//******************************* */
+
+// Define function that prints the argument passed
+function printName(name){
+    console.log('printName function executed'); // print to see function execute
+    console.log('name:',name);
+}
+
+function printAge(age){
+    console.log('printAge function executed'); // print to see function execute
+    console.log('age:',age);
+    return; // end function without returning anything
+    console.log('This will not be executed.') // This statement not executed
+}
+
+var var1 = 'John'; // assign string 'John' to var1
+var var2 = 12; // assign 12 to var2
+var var3 = printName(var1); // assign printName output to var3
+console.log(var3); // print the value of var3
+var var3 = printAge(var2); // assign printAge output to var3
+console.log(var3); // print the value of var3
