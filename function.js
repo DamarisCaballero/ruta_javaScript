fa3eedd8be76508c1de71b81d272ba83c9a59e15
DamarisCaballero <damariscallero011@gmail.com>
2023-05-22 13:59:08 -0600

    function

---
+++ function.js
@@ -0,0 +1,10 @@
+function sum(a , b){
+    console.log('Taking sum in the function'); // print to see function execute
+    var ans = a + b; 
+    return ans;
+}
+
+var var1 = 5; 
+var var2 = 6;
+var var3 = sum(var1, var2);
+console.log(var3);
