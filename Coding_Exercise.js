af7d5721401b741ca07468e06eb3c052fede2410
DamarisCaballero <damariscallero011@gmail.com>
2023-05-22 15:08:26 -0600

    ejercice

--- Coding_Exercise.js
+++ Coding_Exercise.js
@@ -10,3 +10,17 @@ var numero1 = 10;
 var numero2 = 7;
 var resultado = min(numero1, numero2);
 console.log("el numero minimo es: " + resultado);
+
+/**************************************/
+/**************************************/
+
+function min (n1, n2){
+    if (n1 < n2){
+        console.log(n1);
+    } else {
+        console.log(n2);
+    }
+    min(10,3);
+}
+/**************************************/
+/**************************************/
